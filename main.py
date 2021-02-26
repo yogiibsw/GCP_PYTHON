b4e75dca18c0f33f10ae165c4bc84b2a48e11081
YOGI <yogibca62@gmail.com>
2021-02-26 17:49:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello Gcp class'
+    return 'Hello New World'
 
 
 if __name__ == '__main__':
